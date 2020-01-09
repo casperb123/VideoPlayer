0ce534c4d065867dfd60db049c8f513ba0dc62a0
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2020-01-09 11:31:06 +0100

    Changed salt

--- VideoPlayer.Entities/Settings.cs
+++ VideoPlayer.Entities/Settings.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,7 +65,7 @@ namespace VideoPlayer.Entities
 		{
 			Color = 1;
 			Theme = 0;
-			Salt = Protect(Guid.NewGuid().ToByteArray());
+			Salt = Protect(Encoding.ASCII.GetBytes(Environment.UserName));
 			Save().ConfigureAwait(false);
 		}
 
